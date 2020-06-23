e0c1b80d885fd8ec8dacf0a9702556db871ef603
jangkyungdo <rudeh0926@naver.com>
2020-06-23 21:46:47 +0900

    React movie 스타일링

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie(props) {
-  const { id, title, year, summary, poster, rating } = props;
-  return <h1>{title}</h1>;
+  const { title, year, summary, poster, rating, genres } = props;
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h4 className="movie__year">{year}</h4>
+        <h5 className="movie__rating">평점 : {rating} / 10.0</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="movie__genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
