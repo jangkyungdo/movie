0f99f99308c30900bc8594cc1122f32c987ac94f
jangkyungdo <rudeh0926@naver.com>
2020-07-01 18:14:45 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.78531e5ac61ec078810325c748251d82.js"
+  "/movie_app/precache-manifest.6f55025d264ad20a7c1beba682dda83c.js"
 );
 
 self.addEventListener('message', (event) => {
